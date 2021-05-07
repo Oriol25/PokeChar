42bd5539a4feb25dda0dd09d004adaeae0923176
Oriol25 <opoveda@insbaixcamp.cat>
2021-05-07 19:53:44 +0200

    Pokedex Data

---
+++ app/src/main/java/org/insbaixcamp/pokechar/ui/PokedexData.java
@@ -0,0 +1,88 @@
+package org.insbaixcamp.pokechar.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.insbaixcamp.pokechar.R;
+import org.insbaixcamp.pokechar.model.PokedexBasic;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class PokedexData extends AppCompatActivity {
+
+    private String urlData;
+
+    private String TAG = "PokeChar/PokedexData";
+
+    private TextView tvName;
+    private ImageView ibFoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokedex_data);
+
+        Bundle bundle = getIntent().getExtras();
+        urlData = bundle.getString("urlData");
+
+        tvName = findViewById(R.id.tvNamePokedexData);
+        ibFoto = findViewById(R.id.ibFotoPokedexData);
+
+
+        cargarJSON();
+
+
+    }
+
+    public void cargarJSON() {
+        cargarPokemon();
+
+    }
+
+    public void cargarPokemon() {
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                urlData,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+                            String str =response.getString("name");
+                            str = str.toUpperCase().charAt(0) + str.substring(1).toLowerCase();
+                            tvName.setText(str);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i(TAG, error.getMessage());
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
+
+    }
+
+}
