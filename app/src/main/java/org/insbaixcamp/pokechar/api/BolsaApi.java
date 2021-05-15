1b92994599539d0c9d93519f868186f6d18d5779
=Oriol25 <=oriol.poveda@insbaixcamp.cat>
2021-05-15 03:23:54 +0200

    Mejora url Bolsa

--- app/src/main/java/org/insbaixcamp/pokechar/api/BolsaApi.java
+++ app/src/main/java/org/insbaixcamp/pokechar/api/BolsaApi.java
@@ -3,23 +3,12 @@ package org.insbaixcamp.pokechar.api;
 public class BolsaApi {
     public static String bolsa = "https://pokeapi.co/api/v2/item?";
 
-    public static int offsetPokeball = 0;
-    public static int limitPokeball = 16;
-
-    public static int offsetBotiquin = 16;
-    public static int limitBotiquin = 13;
-
-    public static int offsetRemedios = 29;
-    public static int limitRemedios = 8;
-
-    public static int offsetObjetos = 19;
-    public static int limitObjetos = 37;
-
-    public static int offsetSpray = 6;
-    public static int limitSpray = 56;
-
-    public static int offsetAttack = 304;
-    public static int limitAttack = 100;
+    public static String pokeBall = "https://pokeapi.co/api/v2/item?limit=16&offset=0";
+    public static String botiquin = "https://pokeapi.co/api/v2/item?limit=13&offset=16";
+    public static String remedios = "https://pokeapi.co/api/v2/item?limit=8&offset=29";
+    public static String objetoPotenciador = "https://pokeapi.co/api/v2/item?limit=19&offset=37";
+    public static String sprayPotenciador = "https://pokeapi.co/api/v2/item?limit=6&offset=56";
+    public static String tmHm = "https://pokeapi.co/api/v2/item?limit=100&offset=304";
 
     public static String tmSprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/tm-normal.png";
     public static String hmSprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/hm-normal.png";
